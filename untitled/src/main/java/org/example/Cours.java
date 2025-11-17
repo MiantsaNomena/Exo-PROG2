a984838fefbcf38d887677a37db2359d9377e92f
miantsa <hei.miantsa.3@gmail.com>
2025-11-17 23:50:59 +0300

    Ajout projet

---
+++ untitled/src/main/java/org/example/Cours.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Cours {
+    private int id;
+    private String label;
+    private int description;
+    private Teacher teacher;
+}
---
+++ untitled/src/main/java/org/example/Exam.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.time.Instant;
+
+public class Exam {
+    private int id;
+    private String title;
+    private Cours course;
+    private Instant examDateTime;
+    private double coefficient;
+}
